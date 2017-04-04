2ceec84189a7f81f4416e7d95bd60831bd97eac4
Ben Olive <ben.olive@salesloft.com>
2017-04-04 14:04:00 -0400

    Cache to file instead of ENV variable
    This shares the cached results between shells and expires the cache
    afetr an hour. This cache can be cleared by running:
    
    ```
    rm /tmp/_opsworks_instances.*
    ```

--- setup.bash
+++ setup.bash
@@ -3,9 +3,20 @@
 # Add the awssh bin to PATH
 PATH=$PATH:"$(cd "`dirname ${BASH_SOURCE[0]}`" && echo $PWD)/bin"
 
+_CACHED_OPSWORKS_INSTANCES() {
+  # Delete cache older than an hour
+  find /tmp/ -name '_opsworks_instances.*'  -mtime +1h -maxdepth 1 -type f -delete
+  local _CACHE_FILE="$(ls -t /tmp/_opsworks_instances.* 2> /dev/null | head -n 1)"
+  if [[ -z "$_CACHE_FILE" || ! -s "$_CACHE_FILE" ]]; then
+    local _CACHE_FILE="$(mktemp /tmp/_opsworks_instances.XXX)"
+    awssh --list > "$_CACHE_FILE"
+  fi
+  cat "$_CACHE_FILE"
+}
+
 # Setup tab completion for instances
 _awssh_completion() {
-  local INSTANCES="${_OPSWORKS_INSTANCES:=$(awssh --list)}"
+  local INSTANCES="$(_CACHED_OPSWORKS_INSTANCES)"
   local prefix="${COMP_WORDS[COMP_CWORD]}"
   COMPREPLY=( $(compgen -W "$INSTANCES" -- $prefix) )
   return 0
